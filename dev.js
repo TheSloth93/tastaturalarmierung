49511a465f85c55bcb77a9d79057798213a11dac
ChaosKai <kai-waggeling@web.de>
2016-10-13 01:48:03 +0200

    Update dev.js

--- dev.js
+++ dev.js
@@ -168,7 +168,7 @@ $(function(){
             var table = $("#vehicle_show_table_body_all").length ? "#vehicle_show_table_body_all tr":"#vehicle_show_table_rett tr";
 
             // - - - - - Enter  - - - - -
-            if(event.altKey == getKey("key_submit").altKey && event.ctrlKey == getKey("key_submit").ctrlKey && event.metaKey == getKey("key_submit").metaKey && event.which == getKey("key_submit").key)
+            if(event.altKey == getKeyStorage("key_submit").altKey && event.ctrlKey == getKeyStorage("key_submit").ctrlKey && event.metaKey == getKeyStorage("key_submit").metaKey && event.which == getKeyStorage("key_submit").key)
             {
                 if($( "#search_vehicle" ).val() != "")
                 {
