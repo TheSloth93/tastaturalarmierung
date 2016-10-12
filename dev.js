7032e87835287a5f0992f245c522552de498ad35
ChaosKai <kai-waggeling@web.de>
2016-10-12 21:58:45 +0200

    Update dev.js

--- dev.js
+++ dev.js
@@ -18,7 +18,7 @@ html = html + '         <span class="glyphicon glyphicon-option-vertical"></span
 html = html + '      </a>';
 html = html + '   </div>';
 html = html + '   <div style="width: 100%; padding-bottom: 6px">';
-html = html + '      <font style="font-size: 12px">by ChaosKai93 (build 2016-10-12-2140)</font><a href="https://github.com/ChaosKai/tastaturalarmierung" target="_blank" style="font-size: 12px; margin-left: 24px">GitHub Projekt</a>';
+html = html + '      <font style="font-size: 12px">by ChaosKai93 (build 2016-10-12-2158)</font><a href="https://github.com/ChaosKai/tastaturalarmierung" target="_blank" style="font-size: 12px; margin-left: 24px">GitHub Projekt</a>';
 html = html + '   </div>';
 html = html + '</div>';
 
