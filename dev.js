e533d1d39c7f3dd994acf73f303eca8cd9367ec7
ChaosKai <kai-waggeling@web.de>
2016-10-14 00:43:36 +0200

    Update dev.js

--- dev.js
+++ dev.js
@@ -3,6 +3,81 @@ var set_key_name = "";
 
 keyCodeToChar = {8:"Backspace",9:"Tab",13:"Enter",16:"Shift",17:"Ctrl",18:"Alt",19:"Pause/Break",20:"Caps Lock",27:"Esc",32:"Space",33:"Page Up",34:"Page Down",35:"End",36:"Home",37:"Left",38:"Up",39:"Right",40:"Down",45:"Insert",46:"Delete",48:"0",49:"1",50:"2",51:"3",52:"4",53:"5",54:"6",55:"7",56:"8",57:"9",65:"A",66:"B",67:"C",68:"D",69:"E",70:"F",71:"G",72:"H",73:"I",74:"J",75:"K",76:"L",77:"M",78:"N",79:"O",80:"P",81:"Q",82:"R",83:"S",84:"T",85:"U",86:"V",87:"W",88:"X",89:"Y",90:"Z",91:"Windows",93:"Right Click",96:"Numpad 0",97:"Numpad 1",98:"Numpad 2",99:"Numpad 3",100:"Numpad 4",101:"Numpad 5",102:"Numpad 6",103:"Numpad 7",104:"Numpad 8",105:"Numpad 9",106:"Numpad *",107:"Numpad +",109:"Numpad -",110:"Numpad .",111:"Numpad /",112:"F1",113:"F2",114:"F3",115:"F4",116:"F5",117:"F6",118:"F7",119:"F8",120:"F9",121:"F10",122:"F11",123:"F12",144:"Num Lock",145:"Scroll Lock",182:"My Computer",183:"My Calculator",186:";",187:"=",188:",",189:"-",190:".",191:"/",192:"`",219:"[",220:"\\",221:"]",222:"'"};
 
+keyList = {
+    "submit":
+    {
+        "name": "Alarmieren",
+        "description": "Alarmiert die ausgewählten Fahrzeuge",
+        "default": 13
+    },
+    "delete":
+    {
+        "name": "Suchfeld leeren",
+        "description": "Das Eingabefeld zur Suche leeren",
+        "default": 8
+    },
+    "select_all":
+    {
+        "name": "Alle markieren",
+        "description": "Alle sichtbaren Fahrzeuge markieren",
+        "default": 106
+    },
+    "fhz1":
+    {
+        "name": "Fahrzeug 1 markieren",
+        "description": "Das 1. sichtbare Fahrzeug markieren",
+        "default": 49
+    },
+    "fhz2":
+    {
+        "name": "Fahrzeug 2 markieren",
+        "description": "Das 2. sichtbare Fahrzeug markieren",
+        "default": 50
+    },
+    "fhz3":
+    {
+        "name": "Fahrzeug 3 markieren",
+        "description": "Das 3. sichtbare Fahrzeug markieren",
+        "default": 51
+    },
+    "fhz4":
+    {
+        "name": "Fahrzeug 4 markieren",
+        "description": "Das 4. sichtbare Fahrzeug markieren",
+        "default": 52
+    },
+    "fhz5":
+    {
+        "name": "Fahrzeug 5 markieren",
+        "description": "Das 5. sichtbare Fahrzeug markieren",
+        "default": 53
+    },
+    "fhz6":
+    {
+        "name": "Fahrzeug 6 markieren",
+        "description": "Das 6. sichtbare Fahrzeug markieren",
+        "default": 54
+    },
+    "fhz7":
+    {
+        "name": "Fahrzeug 7 markieren",
+        "description": "Das 7. sichtbare Fahrzeug markieren",
+        "default": 55
+    },
+    "fhz8":
+    {
+        "name": "Fahrzeug 8 markieren",
+        "description": "Das 8. sichtbare Fahrzeug markieren",
+        "default": 56
+    },
+    "fhz9":
+    {
+        "name": "Fahrzeug 9 markieren",
+        "description": "Das 9. sichtbare Fahrzeug markieren",
+        "default": 57
+    }
+};
+
 // - -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 // -             Check for jQuery
 // - -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
@@ -12,76 +87,11 @@ if(typeof(jQuery) == 'undefined')
    throw new Error("LSS Tastaturalarmierung: missing jQuery");
 }
 
-// - -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-// -             Create local Storage
-// - -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-
-// - - - - - Submit - - - - -
-if (localStorage.getItem("key_submit") === null) {
-    setKeyStorage("key_submit", 13);
-}
-showKeyCode("submit");
-
-// - - - - - Select All - - - - -
-if (localStorage.getItem("key_select_all") === null) {
-    setKeyStorage("key_select_all", 106);
-}
-showKeyCode("key_select_all");
-
-// - - - - - Delete - - - - -
-if (localStorage.getItem("key_delete") === null) {
-    setKeyStorage("key_delete", 8);
-}
-showKeyCode("delete");
-
-// - - - - - FHZ 1 - - - - -
-if (localStorage.getItem("key_fhz1") === null) {
-    setKeyStorage("key_fhz1", 49);
-}
-
-// - - - - - FHZ 2 - - - - -
-if (localStorage.getItem("key_fhz2") === null) {
-    setKeyStorage("key_fhz2", 50);
-}
-
-// - - - - - FHZ 3 - - - - -
-if (localStorage.getItem("key_fhz3") === null) {
-    setKeyStorage("key_fhz3", 51);
-}
-
-// - - - - - FHZ 4 - - - - -
-if (localStorage.getItem("key_fhz4") === null) {
-    setKeyStorage("key_fhz4", 52);
-}
-
-// - - - - - FHZ 5 - - - - -
-if (localStorage.getItem("key_fhz5") === null) {
-    setKeyStorage("key_fhz5", 53);
-}
-
-// - - - - - FHZ 6 - - - - -
-if (localStorage.getItem("key_fhz6") === null) {
-    setKeyStorage("key_fhz6", 54);
-}
-
-// - - - - - FHZ 7 - - - - -
-if (localStorage.getItem("key_fhz7") === null) {
-    setKeyStorage("key_fhz7", 55);
-}
-
-// - - - - - FHZ 8 - - - - -
-if (localStorage.getItem("key_fhz8") === null) {
-    setKeyStorage("key_fhz8", 56);
-}
-
-// - - - - - FHZ 9 - - - - -
-if (localStorage.getItem("key_fhz9") === null) {
-    setKeyStorage("key_fhz9", 57);
-}
-
-// - -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// - = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+// -
 // -             Create HTML Code
-// - -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// -
+// - = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 var html = '';
 html = html + '<div style="width: 100%; padding-bottom: 24px">';
@@ -132,12 +142,53 @@ html = html + '      <font style="font-size: 12px">by ChaosKai93 (build 2016-10-
 html = html + '   </div>';
 html = html + '</div>';
 
-$( "#mission-form" ).before(html);
-$( "#search_vehicle" ).focus();
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // -             Write Code to Browser
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    $( "#mission-form" ).before(html);
+    $( "#search_vehicle" ).focus();
 
-// - -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-// -             Main function
-// - -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+// - = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+// -
+// -             Add Keys to Content
+// -
+// - = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+    // - - - - - Loop throug Keys - - - - -
+    $.each(keyList, function(key, value) {
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // -             Create HTML for Key
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        html = '';
+        html = html + '<tr>';
+        html = html + '  <td style="text-align: left"><font style="font-weight: 700" title="' + value.description + '">' + value.name + '</font></td>';
+        html = html + '  <td style="text-align: left"><font id="' + key + '_key_code"></font></td>';
+        html = html + '  <td style="text-align: right">';
+        html = html + '    <a href="#" class="btn btn-success navbar-btn btn-sm" style="margin: 0" onclick="setKey(' + "'" + key + "'" + ')">Taste belegen</a>';
+        html = html + '    <a href="#" class="btn btn-danger navbar-btn btn-sm" style="margin: 0" onclick="delKey(' + "'" + key + "'" + ')">Belegung löschen</a>';
+        html = html + '  </td>';
+        html = html + '</tr>';
+
+        $( "#key_settings_table" ).find("tbody").append(html);
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // -             Create default if not exist
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        if (localStorage.getItem("key_" + key) === null) {
+            setKeyStorage(key, value.key);
+        }
+
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // -             Read Key-Code from Storage
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        showKeyCode(key);
+    });
+
+// - = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+// -
+// -             Main Function
+// -
+// - = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 $(function(){
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -156,15 +207,7 @@ $(function(){
             $("#key_settings_button").removeClass("btn-danger").addClass("btn-success");
         }
     });
-   
-    // - = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-    // -
-    // -             Set Keys
-    // -
-    // - = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-   
     
-   
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     // -             Update Table on Key Input
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -297,10 +340,7 @@ $(function(){
     {
         if(set_key_mode)
         {
-            console.log(event.altKey,event.ctrlKey,event.metaKey,event.which)
-            setKeyStorage("key_" + set_key_name, event.which);
-           
-            console.log("set key '" + set_key_name + "' to " + event.which);
+            setKeyStorage(set_key_name, event.which);
 
             $( "#key_settings_console" ).find("font").text("Die Taste wurde gespeichert!");
             window.setTimeout(function() { $( "#key_settings_console" ).find("font").text("") }, 1500);
@@ -382,8 +422,7 @@ function updateTable()
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     function setKeyStorage(key,value)
     {
-        localStorage.setItem(key,value);
-        console.log("set key '" + key + "' to '" + value + "'");
+        localStorage.setItem("key_" + key,value);
         showKeyCode(key);
     }
 
