e6c515989111f51e1808c759a5230367e25f84dc
ChaosKai <kai-waggeling@web.de>
2017-01-09 21:36:38 +0100

    changed filename
    changed filename because tampermonkey and greasemonkey need .user.js in filename

--- tastaturalarmierung.user.js
+++ tastaturalarmierung.user.js
@@ -9,5 +9,5 @@
 
 var scriptElement = document.createElement("script");
 scriptElement.type = "text/javascript";
-scriptElement.src = "https://rawgit.com/ChaosKai/tastaturalarmierung/master/dev.js";
+scriptElement.src = "https://rawgit.com/ChaosKai/tastaturalarmierung/master/tastaturalarmierung.dev.js";
 document.body.appendChild(scriptElement);
